6d798380d999c96ef5615c5801ef55885e9eb236
Haroon Frotan <143062985+HaroonFro@users.noreply.github.com>
2023-11-12 17:28:40 -0500

    Add files via upload

---
+++ Seattle/script.js
@@ -0,0 +1,9 @@
+let district1button = document.getElementsByClassName('dis1')
+
+for (let i = 0; i < district1button.length; i++) {
+    district1button[i];
+
+    district1button.addEventListener('click', () => {
+console.log('Im clicked')
+})
+}
